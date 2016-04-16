0f4cabbc48b4fca3a3633175a97614f00bbd3f87
Koji KUWABARA <quaynst@gmail.com>
2016-04-16 23:47:15 +0900

    simple toggle images

--- dokkiri/ViewController.m
+++ dokkiri/ViewController.m
@@ -14,9 +14,25 @@
 
 @implementation ViewController
 
+{
+    __weak IBOutlet UIImageView *image;
+//    IBOutlet UIImageView *image_after;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self performSelector:@selector(change) withObject:nil afterDelay:5.0];
+    // 5秒後に change を実行
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)change {
+    
+    image.image = [UIImage
+                   imageNamed:@"snoopy-02.jpg"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
